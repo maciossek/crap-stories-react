74d800d6b704bcb41bceb4da83836552c13e11c6
Mathias Maciossek <mathias.maciossek@gmail.com>
2023-04-02 17:39:40 +0200

    Update antd and libs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.less";
+import { ConfigProvider } from "antd";
 import { ThemeProvider } from "@emotion/react";
 import defaultTheme from "./themes/default";
 import RootRoutes from "./routes/root.routes";
 
 export default function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <RootRoutes />
-    </ThemeProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: defaultTheme.colors.primary500,
+        },
+      }}
+    >
+      <ThemeProvider theme={defaultTheme}>
+        <RootRoutes />
+      </ThemeProvider>
+    </ConfigProvider>
   );
 }
