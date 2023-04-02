e090e5eb4b0f97987d0b5826ac2bee5081d1f24a
Mathias Maciossek <mathias.maciossek@gmail.com>
2023-04-02 18:51:24 +0200

    Update antd and libs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.less";
+import { ConfigProvider } from "antd";
 import { ThemeProvider } from "@emotion/react";
 import defaultTheme from "./themes/default";
 import RootRoutes from "./routes/root.routes";
@@ -15,11 +16,19 @@ export default function App() {
   return (
     <ApolloProvider client={apClient()}>
       <ThemeProvider theme={defaultTheme}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <RootRoutes />
-          </PersistGate>
-        </Provider>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: defaultTheme.colors.primary500,
+            },
+          }}
+        >
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <RootRoutes />
+            </PersistGate>
+          </Provider>
+        </ConfigProvider>
       </ThemeProvider>
     </ApolloProvider>
   );
